6a5247dcb76ca73172e6874873f565067e7d36a5
sauravbv <41701284+sauravbv@users.noreply.github.com>
2019-05-01 14:51:13 +0530

    apk running trial

--- app/src/main/java/com/example/vdetails/vehicleDetails.java
+++ app/src/main/java/com/example/vdetails/vehicleDetails.java
@@ -1,6 +1,13 @@
 package com.example.vdetails;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class vehicleDetails extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vehicle_details);
+    }
 }
