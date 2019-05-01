34b66b59036603567b22d9e22f85f8af51cd74e4
Yaswanth3277 <yashjana@gmail.com>
2019-05-01 15:05:55 +0530

    Fifth Commit

--- app/src/main/java/com/example/vdetails/vehicleDetails.java
+++ app/src/main/java/com/example/vdetails/vehicleDetails.java
@@ -3,11 +3,19 @@ package com.example.vdetails;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class vehicleDetails extends AppCompatActivity {
+
+    public TextView Registration_Number;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vehicle_details);
+        Bundle b = this.getIntent().getExtras();
+        String Vehicle_number = b.getString("Vehicle_Number");
+        Registration_Number = findViewById(R.id.Regno);
+        Registration_Number.setText(Vehicle_number);
+
     }
 }
