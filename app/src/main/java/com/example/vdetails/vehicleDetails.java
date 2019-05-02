b2d533289e3cca43125e1dd7d5cae0b0715f9683
Shayan <shayan1232001@yahoo.co.in>
2019-05-02 17:33:54 +0530

    real stuff

--- app/src/main/java/com/example/vdetails/vehicleDetails.java
+++ app/src/main/java/com/example/vdetails/vehicleDetails.java
@@ -16,5 +16,50 @@ public class vehicleDetails extends AppCompatActivity {
         Registration_Number = findViewById(R.id.Regno);
         Registration_Number.setText(Vehicle_number);
 
+        try{
+
+            String host = "www.regcheck.org.uk";
+            Socket socket = new Socket(host, 80);
+// change yourusernamehere
+            String request = "GET http://www.regcheck.org.uk/api/reg.asmx/CheckIndia?RegistrationNumber=" + Registration_Number + "&username=sauravbv HTTP/1.0\r\n\r\n";
+            OutputStream os = socket.getOutputStream();
+            os.write(request.getBytes());
+            os.flush();
+
+            InputStream in = socket.getInputStream();
+            StringBuilder sb=new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String read;
+
+            while((read=br.readLine()) != null) {
+//System.out.println(read);
+                sb.append(read);
+            }
+
+            br.close();
+            String strXml = sb.toString();
+            int intStart = strXml.indexOf("<?xml");
+            strXml = strXml.substring(intStart);
+//System.out.print(strXml);
+            socket.close();
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            ByteArrayInputStream input = new ByteArrayInputStream(strXml.getBytes("UTF-8"));
+            Document doc = builder.parse(input);
+            NodeList nList = doc.getElementsByTagName("vehicleJson");
+            Node nNode = nList.item(0);
+            //System.out.print(nNode.getTextContent());
+            JSONObject reader = new JSONObject(nNode.getTextContent());
+
+            //JSONObject sys  = reader.getJSONObject("Description");
+            Description = sys.getString("Description");
+
+        }
+        catch(Exception ex)
+        {
+            System.out.print("Error");
+        }
+
     }
 }
