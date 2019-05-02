a70486336b6d4156df83e6ab8a9f5722a162a2f5
menahishayan <menahi.shayan@gmail.com>
2019-05-02 19:14:22 +0530

    async

--- app/src/main/java/com/example/vdetails/vehicleDetails.java
+++ app/src/main/java/com/example/vdetails/vehicleDetails.java
@@ -47,6 +47,7 @@ public class vehicleDetails extends AppCompatActivity {
         get_details = findViewById(R.id.button5);
         Log.d("Test", "Testing Debug");
 
+        new TestAsync().execute();
 
     }
 
@@ -112,5 +113,79 @@ class Vdetails extends AsyncTask<String, Void, Void> {
         protected void onPostExecute (RSSFeed feed){
 
         }
+    }
+}
+
+class TestAsync extends AsyncTask<Void, Integer, String>
+{
+    String TAG = getClass().getSimpleName();
+
+    protected void onPreExecute (){
+        super.onPreExecute();
+        Log.d(TAG + " PreExceute","On pre Exceute......");
+    }
+
+    protected String doInBackground(Void...arg0) {
+        Log.d(TAG + " DoINBackGround","On doInBackground...");
+
+        try {
+
+            String host = "www.regcheck.org.uk";
+            Socket socket = new Socket(host, 80);
+
+            String request = "GET http://www.regcheck.org.uk/api/reg.asmx/CheckIndia?RegistrationNumber=KA05KG2990&username=sauravbv HTTP/1.0\r\n\r\n";
+            OutputStream os = socket.getOutputStream();
+            os.write(request.getBytes());
+            os.flush();
+
+            InputStream in = socket.getInputStream();
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String read;
+
+            while ((read = br.readLine()) != null) {
+
+                sb.append(read);
+            }
+
+            br.close();
+
+
+            String strXml = sb.toString();
+            Log.d("str", strXml);
+            int intStart = strXml.indexOf("<?xml");
+            strXml = strXml.substring(intStart);
+
+            socket.close();
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            ByteArrayInputStream input = new ByteArrayInputStream(strXml.getBytes("UTF-8"));
+            Document doc = builder.parse(input);
+            NodeList nList = doc.getElementsByTagName("vehicleJson");
+            Node nNode = nList.item(0);
+            Log.d("Output", nNode.getTextContent());
+            JSONObject reader = new JSONObject(nNode.getTextContent());
+
+            //JSONObject sys  = reader.getString("Description");
+            //Desc = reader.getString("Description");
+            Desc = nNode.getTextContent();
+            Vehicle_num.setText(Desc);
+        } catch (Exception ex) {
+            Log.d("Error", ex.toString());
+        }
+        Log.d("output", Desc);
+
+        return "You are at PostExecute";
+    }
+
+    protected void onProgressUpdate(Integer...a){
+        super.onProgressUpdate(a);
+        Log.d(TAG + " onProgressUpdate", "You are in progress update ... " + a[0]);
+    }
+
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Log.d(TAG + " onPostExecute", "" + result);
     }
 }
