0975d0216bdcd624372707e639d1fa67609db303
Yaswanth3277 <yashjana@gmail.com>
2019-05-02 16:00:41 +0530

    02-05-2019

--- app/src/main/java/com/example/vdetails/uploadPic.java
+++ app/src/main/java/com/example/vdetails/uploadPic.java
@@ -22,7 +22,7 @@ public class uploadPic extends Activity {
     Bitmap bitmap;
 
 
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +30,15 @@ public class uploadPic extends Activity {
 
         OcrManager manager = new OcrManager();
         manager.initAPI();
-        Button buttonLoadImage = (Button)findViewById(R.id.loadimage);
-        textTargetUri = (TextView)findViewById(R.id.targeturi);
-        targetImage = (ImageView)findViewById(R.id.targetimage);
+        Button buttonLoadImage = findViewById(R.id.loadimage);
+        textTargetUri = findViewById(R.id.targeturi);
+        targetImage = findViewById(R.id.targetimage);
 
         buttonLoadImage.setOnClickListener(new Button.OnClickListener(){
 
             @Override
             public void onClick(View arg0) {
-                // TODO Auto-generated method stub
+
                 Intent intent = new Intent(Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, 0);
@@ -47,7 +47,7 @@ public class uploadPic extends Activity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
+
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK){
@@ -58,7 +58,7 @@ public class uploadPic extends Activity {
                 bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
                 targetImage.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
+
                 e.printStackTrace();
             }
         }
