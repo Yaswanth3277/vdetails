75eaf40eac41411f37b06b5ea82622f8b7b3df41
Yaswanth3277 <yashjana@gmail.com>
2019-05-02 19:09:56 +0530

    recent

--- app/src/main/java/com/example/vdetails/vehicleDetails.java
+++ app/src/main/java/com/example/vdetails/vehicleDetails.java
@@ -1,7 +1,9 @@
 package com.example.vdetails;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,8 @@ import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -18,13 +22,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class vehicleDetails extends AppCompatActivity {
 
-    String Desc;
+    String Desc = "";
     public TextView Registration_Number;
     public EditText Vehicle_num;
     public Button get_details;
@@ -38,10 +45,22 @@ public class vehicleDetails extends AppCompatActivity {
         Registration_Number.setText(Vehicle_number);
         Vehicle_num = findViewById(R.id.Vnum);
         get_details = findViewById(R.id.button5);
+        Log.d("Test", "Testing Debug");
+
 
     }
-    public void getNumber(View view){
-        try{
+
+
+
+}
+
+class Vdetails extends AsyncTask<String, Void, Void> {
+
+    private Exception exception;
+
+    protected Void doInBackground(String... urls) {
+
+        try {
 
             String host = "www.regcheck.org.uk";
             Socket socket = new Socket(host, 80);
@@ -52,17 +71,20 @@ public class vehicleDetails extends AppCompatActivity {
             os.flush();
 
             InputStream in = socket.getInputStream();
-            StringBuilder sb=new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             String read;
 
-            while((read=br.readLine()) != null) {
+            while ((read = br.readLine()) != null) {
 
                 sb.append(read);
             }
 
             br.close();
+
+
             String strXml = sb.toString();
+            Log.d("str", strXml);
             int intStart = strXml.indexOf("<?xml");
             strXml = strXml.substring(intStart);
 
@@ -74,17 +96,21 @@ public class vehicleDetails extends AppCompatActivity {
             Document doc = builder.parse(input);
             NodeList nList = doc.getElementsByTagName("vehicleJson");
             Node nNode = nList.item(0);
-            //System.out.print(nNode.getTextContent());
+            Log.d("Output", nNode.getTextContent());
             JSONObject reader = new JSONObject(nNode.getTextContent());
 
             //JSONObject sys  = reader.getString("Description");
             //Desc = reader.getString("Description");
             Desc = nNode.getTextContent();
+            Vehicle_num.setText(Desc);
+        } catch (Exception ex) {
+            Log.d("Error", ex.toString());
         }
-        catch(Exception ex)
-        {
-            Vehicle_num.setText(ex.toString());
+        Log.d("output", Desc);
+
+
+        protected void onPostExecute (RSSFeed feed){
+
         }
-        Vehicle_num.setText(Desc);
     }
 }
